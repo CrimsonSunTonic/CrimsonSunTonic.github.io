8062ce5b3085b09d7d04e19cdaec20ca67e9699e
nhatcomnhom <nhatcomnhom@gmail.com>
2025-11-10 03:00:31 +0900

    full moon chord lib add

--- _posts/2024-03-16-ArimaKana-Full-Moon...!.markdown
+++ _posts/2024-03-16-ArimaKana-Full-Moon...!.markdown
@@ -8,33 +8,39 @@ tuning: "Standard EADGBE - Capo 1"
 youtube: "https://www.youtube.com/embed/YGin8rAZ3Mw"
 ---
 
-[Intro]<br>
+<!-- # Intro -->
+
 白く[Cadd9]眩[Dsus4]しく <br>
-あなた[Bm7b13]の心[Em7]を奪う <br>
-[Am7]光[D9sus4]になり[Cadd9]たい <br><br>
+あなた[Bm7b13]の心[Em7(v2)]を奪う <br>
+[Am7(v2)]光[D9sus4]になり[Cadd9]たい <br><br>
 
-[D Bm7b13 Em7 Am7 D9sus4 Gsus4 → G]
+[D] [Bm7b13] [Em7(v2)] [Am7(v2)] [D9sus4] [Gsus4] [G(v2)]
 
-[Verse]<br>
-[G/B]思い切[Cadd9]り腕[D]広げれ[G]ば <br>
-[G/B]思うま[Cadd9]ま喉[D]震わせれ[Em7]ば <br>
-誰より[G/B]明る[Cadd9]く輝き[D]始め[G]る <br>
-物[G/B]語はい[Cadd9]つでも[D]思うがま[G]ま <br><br>
+<!-- # Verse -->
 
-[Pre-Chorus]<br>
-[Cadd9]流した[Bm7b13] 涙[Am7]達散[G/B]らばる <br>
-[Am7]満天の夜[D9sus4→D7]空 <br><br>
+[G/B]思い切[Cadd9]り腕[D]広げれ[G(v2)]ば <br>
+[G/B]思うま[Cadd9]ま喉[D]震わせれ[Em7(v2)]ば <br>
+誰より[G/B]明る[Cadd9]く輝き[D]始め[G(v2)]る <br>
+物[G/B]語はい[Cadd9]つでも[D]思うがま[G(v2)]ま <br><br>
 
-[Chorus]<br>
-今日は[Cadd9]Full [D]moon　世界[Bm7]中誰も[Em7]が <br>
+<!-- Pre-Chorus -->
+
+[Cadd9]流した[Bm7b13] 涙[Am7(v2)]達散[G/B]らばる <br>
+[Am7(v2)]満天の夜[D9sus4(v2)] [D7(v2)]空 <br><br>
+
+<!-- # Chorus -->
+
+今日は[Cadd9]Full [D]moon　世界[Bm7]中誰も[Em7(v2)]が <br>
 空見[Cadd9]上げ[D]てる　透[B7]明　澄み切った[Em7]舞台 <br>
 星の[Cadd9]降る[D]夜　あなた[D#dim7]に一[Em7]番[G7/D]届く <br>
-[Am7]満[D9sus4]月の[G]光<br><br>
+[Am7(v2)]満[D9sus4]月の[G(v2)]光<br><br>
 
-[Post-Chorus]<br>
-白く[C#m7b5]眩[CM7]しく あなた[Bm7→B7]の心[Esus4→E7]を奪う <br>
-[Am7]光[D9sus4]になり[Cadd9]たい<br><br>
+<!-- # Post-Chorus -->
 
-[Outro]
-[D | Bm7 | Em7 | Am7 | D9sus4 | Gsus4 → G] <br>
+白く[C#m7b5]眩[CM7(v2)]しく あなた[Bm7] [B7(v2)]の心[E7sus4] [E7]を奪う <br>
+[Am7(v2)]光[D9sus4]になり[Cadd9]たい<br><br>
+
+<!-- # Outro -->
+
+[D] [Bm7]  [Em7(v2)] [Am7(v2)] [D9sus4] [Gsus4] [G(v2)] <br>
 (La La La……)<br>
--- assets/js/chord-database.js
+++ assets/js/chord-database.js
@@ -1,10 +1,36 @@
 // Global chord database
 let chordDatabase = {};
 
+// Function to convert string format to array format
+function parseFingersString(fingersString) {
+  const fingers = [];
+  for (let i = 0; i < fingersString.length; i++) {
+    const stringNumber = i + 1; // Strings are 1-indexed
+    const fretChar = fingersString[i];
+    
+    if (fretChar === 'x' || fretChar === 'X') {
+      fingers.push([stringNumber, 'x']);
+    } else {
+      const fret = parseInt(fretChar, 10);
+      if (!isNaN(fret)) {
+        fingers.push([stringNumber, fret]);
+      }
+    }
+  }
+  return fingers;
+}
+
 // Function to load chord data from Jekyll data files
 function loadChordDatabase() {
   // This will be populated by Jekyll data
   if (typeof window.chordData !== 'undefined') {
+    // Convert string format to array format for all chords
+    Object.keys(window.chordData).forEach(chordName => {
+      const chord = window.chordData[chordName];
+      if (typeof chord.fingers === 'string') {
+        chord.fingers = parseFingersString(chord.fingers);
+      }
+    });
     chordDatabase = window.chordData;
   }
 }
@@ -19,8 +45,12 @@ function getChordData(chordName) {
   return chordDatabase[chordName] || null;
 }
 
-// Function to add new chords to database
+// Function to add new chords to database (supports both formats)
 function addChordToDatabase(chordName, chordData) {
+  // Convert string format to array format if needed
+  if (chordData.fingers && typeof chordData.fingers === 'string') {
+    chordData.fingers = parseFingersString(chordData.fingers);
+  }
   chordDatabase[chordName] = chordData;
 }
 
@@ -32,6 +62,19 @@ function chordExists(chordName) {
   }
   
   return chordName in chordDatabase;
+}
+
+// Utility function to convert array format back to string format (for debugging)
+function convertFingersArrayToString(fingersArray) {
+  const result = ['x', 'x', 'x', 'x', 'x', 'x']; // Start with all muted
+  
+  fingersArray.forEach(([stringNum, fret]) => {
+    if (stringNum >= 1 && stringNum <= 6) {
+      result[stringNum - 1] = fret.toString();
+    }
+  });
+  
+  return result.join('');
 }
 
 // Initialize when script loads
